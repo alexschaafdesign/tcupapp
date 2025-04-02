26ff0e34401d3e540be1039808765af5fe927514
Alex Schaaf <alexschaafdesign@gmail.com>
2025-04-02 16:24:19 -0500

    Fixed other merge

--- frontend/src/pages/Chat/Components/Post/PostContent.js
+++ frontend/src/pages/Chat/Components/Post/PostContent.js
@@ -6,18 +6,6 @@ const PostContent = ({ post, renderContent }) => {
   const hasImages = post.images && Array.isArray(post.images) && post.images.length > 0;
   
   return (
-<<<<<<< HEAD
-    <Box sx={{ 
-      width: '100%', 
-      wordBreak: 'break-word', 
-      overflowWrap: 'anywhere',
-      '& p': { 
-        margin: 0,
-        marginBottom: 0
-      },
-      '& br': { display: 'block', content: '""', marginTop: '0.3em' }
-    }}>
-=======
     <Box 
       className="message-content"
       sx={{ 
@@ -25,7 +13,6 @@ const PostContent = ({ post, renderContent }) => {
         wordBreak: 'break-word', 
         overflowWrap: 'anywhere'
       }}>
->>>>>>> dev
       {renderContent && renderContent(post.content)}
       {hasImages && <ImageAttachmentsGrid images={post.images} />}
     </Box>
